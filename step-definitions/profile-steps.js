69d41c023c8a4393102344ab5ddcc66fffe7cba4
Zubair Alam <syedzubairalam123@gmail.com>
2024-11-26 17:47:35 +0500

    fixes

--- step-definitions/profile-steps.js
+++ step-definitions/profile-steps.js
@@ -9,23 +9,23 @@ Then('the additional information banner should be visible', async () => {
   });
 
   When('I enter SSN {string} text', async function (ssn) {
-    await page.fill(loginPage.SSN, ssn);
+    await page.fill(profilePage.SSN, ssn);
   });
 
   When('I enter DATE {string} text', async function (date) {
-    await page.fill(loginPage.DATE, date);
+    await page.fill(profilePage.DATE, date);
   });
 
   When('I click on the {string} button', function (name) {
-    return page.click(loginPage.Button(name));
+    return page.click(profilePage.Button(name));
   });
 
   Then('the {string} button should be disabled again', async function (name) {
-    const isDisabled = await page.locator(loginPage.Button(name)).isDisabled();
+    const isDisabled = await page.locator(profilePage.Button(name)).isDisabled();
     expect(isDisabled).toBe(true);
   });
 
   Then('I should see You have {string} attempts remaining.', async function (name) {
-    await expect(page.locator(loginPage.attemptLeftOnProfileTab(name))).toBeVisible();
+    await expect(page.locator(profilePage.attemptLeftOnProfileTab(name))).toBeVisible();
     await page.waitForTimeout(6000);
   });
